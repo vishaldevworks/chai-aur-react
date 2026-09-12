18d84a9c543292187a1162a8ad9507283dbf1fd9
Vishalpro007 <visal7431@gmail.com>
2026-09-13 03:40:38 +0530

    add vite react setup

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,11 @@
+import Vishal from "./vishal";
+function App() {
+  return (
+    <>
+      <Vishal />
+      <p>test done at end succes</p>
+    </>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/Vishal.jsx
@@ -0,0 +1,7 @@
+function Vishal () {
+    return (
+        <h3>vishal is ready</h3>
+    )
+}
+
+export default Vishal
